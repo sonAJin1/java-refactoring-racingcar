ea7b7e5510d330c30f956c14fefa6560e4d21632
손아진(Zinzer) <ahjin.son@sk.com>
2021-05-06 09:13:47 +0900

    (feature) RacingGame에 CarFactory연결

--- src/main/java/controller/RacingGame.java
+++ src/main/java/controller/RacingGame.java
@@ -1,4 +1,14 @@
 package controller;
 
+import model.racingCar.CarFactory;
+import model.stratege.MoveStrategy;
+import ui.InputView;
+
 public class RacingGame {
+    private CarFactory carFactory;
+
+    public RacingGame(MoveStrategy moveStrategy) {
+        int gameRound = InputView.inputRacingRound();
+        carFactory = new CarFactory(InputView.inputCarCount(), moveStrategy);
+    }
 }
